960037cca4fe9c1634b56da0ecd6b171b35a496d
Dora O <dorozco2390@utexas.edu>
2021-03-19 23:13:50 -0500

    updated line 24 and added app get api/notes function

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,11 @@
 //dependencies
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const dbJSON = require("./db/db.json");
+
+//uuid package installed for unique id for each note
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,14 +16,25 @@ app.use(express.json());
 app.use(express.static('./public'));
 
 //routes to notes html
-app.get('/notes', (req, res) =>{
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/notes.html'));
 });
 
 //routes to index.html
-app.get('/', (req, res) =>{
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
+
+app.get('/api/notes', function (req, res) {
+    fs.readFile('./db/db.json', 'utf8', function (err, data) {
+        res.json(JSON.parser(data));
+    })
+});
+// app.post('/api/notes', function (req, res) {
+//     const note = { ...req.body, id: uuidv4() };
+//     dbJSON.push(note)
+//     fs.writeFile();
+// });
 
 //starts server to begin listening
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));

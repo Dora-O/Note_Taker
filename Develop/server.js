8ab4f15e2ecac3f3e16895f0d3076e77417940f9
Dora O <dorozco2390@utexas.edu>
2021-03-20 01:28:01 -0500

    flipped get app notes and index and removed * and replaced with / and now function can read and write the notes

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const dbJSON = require('./db/db.json');
+const dbJSON = require("./db/db.json");
 
 //uuid package installed for unique id for each note
 const { v4: uuidv4 } = require('uuid');
@@ -15,14 +15,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('./public'));
 
+//routes to index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
 //routes to notes html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-//routes to index.html
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 //reads and returns all saved notes as JSON
